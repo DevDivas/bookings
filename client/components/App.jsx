508e6ee45526295bef817fb9d19d8358f139d139
Allison Chen <allisonc07@gmail.com>
2018-07-25 14:56:13 -0700

    Handle live data fetched from API request in Header and Stars

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,69 @@
+/* eslint-env browser */
+
 import React from 'react';
+import Header from './Header';
+
+const axios = require('axios');
 
 class App extends React.Component {
-  super() {
+  constructor() {
+    super();
     this.state = {
-      month: '',
+      roomid: /\d+/g.exec(window.location.pathname)[0],
+      month: '05',
+      bookings: [],
+      bookingsThisMonth: [],
+      roomDetails: {},
     };
   }
 
+  componentDidMount() {
+    const { roomid, month } = this.state;
+    axios.get(`/rooms/${roomid}/room`)
+      .then((response) => {
+        const roomDetails = response.data[0];
+        this.setState({
+          roomDetails: {
+            roomRateBase: roomDetails.room_rate_base,
+            roomRatepeak: roomDetails.room_rate_peak,
+            cleaningFee: roomDetails.cleaning_fee,
+            serviceFee: roomDetails.service_fee,
+            maxGuests: roomDetails.max_guests,
+            stars: roomDetails.stars,
+          },
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    axios.get(`/rooms/${roomid}/bookings`)
+      .then((response) => {
+        const roomBookings = response.data;
+        const monthlyBookings = roomBookings.reduce((bookingsPerMonth, booking) => {
+          if (booking.start_date.slice(5, 7) === month) {
+            bookingsPerMonth.push(booking);
+          } else if (booking.end_date.slice(5, 7) === month) {
+            bookingsPerMonth.push(booking);
+          }
+          return bookingsPerMonth;
+        }, []);
+        this.setState({
+          bookings: roomBookings,
+          bookingsThisMonth: monthlyBookings,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+
   render() {
+    const { roomDetails, stars } = this.state;
     return (
       <div>
+        <Header roomDetails={roomDetails} stars={stars} />
+        <hr />
       </div>
     );
   }
--- client/components/Stars.jsx
+++ client/components/Stars.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Stars = ({ num }) => Array(5).fill('').map((star, i) => {
+  // temporarily using buttons until I find nice star svgs
   const content = (
     <button type="button">
       O

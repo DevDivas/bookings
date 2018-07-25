3ceb50891688ad63574e6fc61e456909359212b4
Allison Chen <allisonc07@gmail.com>
2018-07-24 21:54:08 -0700

    Refactor Stars

--- client/components/Stars.jsx
+++ client/components/Stars.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 const Stars = ({ num }) => Array(5).fill('').map((star, i) => {
+  const content = (
+    <button type="button">
+      O
+    </button>
+  );
   if (i < num) {
     return (
       <button type="button">
@@ -8,11 +13,7 @@ const Stars = ({ num }) => Array(5).fill('').map((star, i) => {
       </button>
     );
   }
-  return (
-    <button type="button">
-      O
-    </button>
-  );
+  return content;
 });
 
 export default Stars;
